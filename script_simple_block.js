d4ced4ddad705594c9d3d1cf41a269d810d8233b
Lamo <145445658+lamoneko@users.noreply.github.com>
2024-06-13 16:22:29 +0900

    Update script_simple_block.js

--- script_simple_block.js
+++ script_simple_block.js
@@ -1,59 +1,36 @@
-const audioContext = new (window.AudioContext || window.webkitAudioContext)(); //Web Audio APIを使用して音声を操作するためのAudio Contextを作成する
+//Web Audio APIを使用して音声を操作するためのAudio Contextを作成する
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+//キーボードのキーと対応する周波数をマッピングするための'keyMap'を定義
+const keyMap = '1234567890QWERTYUIOPASDFGHJKLZXCVBNM'.split('');
 
-const keyMap = '1234567890QWERTYUIOPASDFGHJKLZXCVBNM'.split(''); //キーボードのキーと対応する周波数をマッピングするための'keyMap'を定義
-
-let frequencies = [];  //生成された周波数を格納するための配列
-
-let oscillators = {};  //オシレーターを管理するためのオブジェクト
-
-let chord = []; //和音を保存するための配列
-
-let chordCounts = {}; ////和音の出現頻度を保存するオブジェクト
+//生成された周波数を格納するための'frequencies'を定義する
+let frequencies = [];
+//オシレーターを管理するためのオブジェクト'oscillators'を定義する
+let oscillators = {};
 
 
-//音を再生するためのオシレーターを作成し，再生する
+//音の再生
 function startPlayingFrequency(frequency, key) {
-
     const oscillator = audioContext.createOscillator();
-    oscillator.type = 'sine'; //オシレーターのタイプをサイン波に設定
-    oscillator.frequency.setValueAtTime(frequency, audioContext.currentTime);
+    oscillator.type = 'sine'; //オシレータのタイプをサイン波に設定
+    oscillator.frequency.setValueAtTime(frequency, audioContext.currentTime); //オシレータの周波数を設定する
     oscillator.connect(audioContext.destination); //オシレーターをオーディオの出力に接続する
-    oscillator.start(); //オシレータを再生する
-    oscillators[key] = oscillator; // オシレータをosillatorsオブジェクトに保存
+    oscillator.start();
 
-    //入力された音をコンソールに表示
-    console.log("Input frequency:",frequency.toFixed(2)); 
-
-    // 和音に周波数を追加する
-    chord.push(frequency.toFixed(2));
-
+    oscillators[key] = oscillator; // オシレーターを保存
+    highlightKey(keyMap.indexOf(key)); //キーを強調表示する
 }
 
-//和音が鳴らされているかどうかをチェックする関数
-function checkChord(){
-    // 現在の和音に既に2つ以上の周波数が含まれている場合、和音として保存
-    if (chord.length >= 2) {
-        chord.push(chord);
-        console.log("Chord:", chord); //コンソールに表示
-
-        ////和音の周波数をソートして一意な識別子に変換
-        const chordIdentifier = chord.sort().join(','); //和音の周波数を昇順にソート＋ソートされた周波数をカンマ区切りの文字列に変換する
-        ////和音の出現頻度をカウントする
-        chordCounts[chordIdentifier] = (chordCounts[chordIdentifier] || 0) + 1; //和音の出現回数を1増やす，和音がカウントされていない場合は0
-        console.log(chordCounts);
-    }
-    chord = []; // 現在の和音をリセット
-}
-
-//指定されたキーに対応するオシレーターの停止，削除
+//音の停止
 function stopPlayingFrequency(key) {
     if (oscillators[key]) {
-        oscillators[key].stop(); 
-        oscillators[key].disconnect(); //オシレーターをオーディオの出力から切断する
-        delete oscillators[key]; //停止したオシレーターを削除する
+        oscillators[key].stop();
+        oscillators[key].disconnect();
+        delete oscillators[key]; //停止したオシレーターを削除
+        removeHighlight(keyMap.indexOf(key));
     }
-    checkChord(); //和音が鳴らされたかをチェック
 }
+
 
 //キーボードのスケールを生成する
 function generateScale() {
@@ -65,22 +42,22 @@ function generateScale() {
         const frequency = rootFrequency * Math.pow(2, i / division);
         frequencies.push(frequency); //計算された周波数をfrequencies配列に追加する
     }
-    updateKeyboardLayout(division); //分割数に応じてキーボードの見た目を更新する
+    updateKeyboardLayout(division); //キーボードの数を更新する
 }
 
 ////ブロック型の作成
-function updateKeyboardLayout(division) { //引数divisionを受け取る(キーボード全体をいくつの部分に分割するかを指定)
-    const notesDiv = document.getElementById('keyboard-container'); //
-    const freqDiv = document.getElementById('frequencies'); //
-    notesDiv.innerHTML = ''; //以前のキーボードレイアウトをクリア(前のレイアウトを消去)
-    freqDiv.innerHTML = ''; // 周波数表示エリアをクリア(前の周波数表示を消去)
+function updateKeyboardLayout(division) {
+    const notesDiv = document.getElementById('keyboard-container');
+    const freqDiv = document.getElementById('frequencies');
+    notesDiv.innerHTML = '';
+    freqDiv.innerHTML = ''; // 周波数表示エリアをクリア
     const keyWidth = 50; // 鍵盤の幅
     const keyHeight = 200; // 鍵盤の高さ
-    const margin =5 ; //鍵盤の間隔
+    const margin = 5; //鍵盤の間隔
 
     for (let i = 0; i < division; i++) {
-        const x = i * (keyWidth+margin); // 鍵盤ごとに横方向に配置
-        const y = 0; //縦（変わらない）
+        const x = i * (keyWidth + margin); // 鍵盤ごとに横方向に配置
+        const y = 0;
 
         const noteDiv = document.createElement('div');
         noteDiv.className = 'note'; //
@@ -99,47 +76,71 @@ function updateKeyboardLayout(division) { //引数divisionを受け取る(キー
     }
 }
 
+//キーがクリック（またはタップ）されると特定の周波数の音を再生
+//オシレーターを作成し、周波数を設定し、オーディオコンテキストの宛先に接続し、再生を開始し、0.5秒後に停止
+function playFrequency(frequency, keyIndex) {
+    const oscillator = audioContext.createOscillator();
+    oscillator.type = 'sine';
+    oscillator.frequency.setValueAtTime(frequency, audioContext.currentTime);
+    oscillator.connect(audioContext.destination);
+    oscillator.start(); //再生
+    oscillator.stop(audioContext.currentTime + 0.5); //0.5秒後に停止
+
+    highlightKey(keyIndex);
+}
+
+
 ////キーボード制御
 //キーが押されたときに呼び出される関数
-function handleKeyDown(event) { 
+function handleKeyDown(event) {
     const key = event.key.toUpperCase(); //イベントから押されたキーを取得し、大文字に変換してkey に格納する。これにより、大文字と小文字のキーの区別をなくす。
     const keyIndex = keyMap.indexOf(key); //
     if (keyIndex >= 0 && keyIndex < frequencies.length && !oscillators[key]) {
         startPlayingFrequency(frequencies[keyIndex], key);
     }
-    
+
     // 押された鍵盤の色を変更する
     const noteDiv = document.getElementsByClassName('note')[keyIndex];
     noteDiv.style.background = 'lightgray'; // 背景色を変更
     noteDiv.style.border = 'solid black 2px'; // 枠の色を変更
-    
+
 }
 
 //キーが離されたときに呼び出される関数
-function handleKeyUp(event) { 
+function handleKeyUp(event) {
     const notes = document.getElementsByClassName('note');
     for (let i = 0; i < notes.length; i++) {
-            notes[i].style.border = 'solid black 1px';
+        notes[i].style.border = 'solid black 1px';
     }
-
     const key = event.key.toUpperCase();
+
     if (oscillators[key]) {
         stopPlayingFrequency(key);
     }
 
-     // 鍵盤の色付けを元に戻す
+    // 鍵盤の色付けを元に戻す
     const keyIndex = keyMap.indexOf(key);
     const noteDiv = document.getElementsByClassName('note')[keyIndex];
     noteDiv.style.background = 'white'; // 背景色を元に戻す
     noteDiv.style.border = 'solid black 1px'; // 枠の色を元に戻す
-    
+
 }
 
+// タッチ対応
+function handleTouchEvents() {
+    const notes = document.getElementsByClassName('note'); //キーボードのキー（クラス名がnoteであるすべての要素）を取得
+    for (let i = 0; i < notes.length; i++) { //forループで各要素に対してaddEventListerメソッドを使用してタッチイベントを追加する
+        notes[i].addEventListener("touchstart", notePressed, false); //キーが押された
+        notes[i].addEventListener("touchend", noteReleased, false); //キーが離された
+        notes[i].addEventListener("touchmove", noteReleased, false); //スライドしてキーが離された
+        notes[i].addEventListener("touchcancel", noteReleased, false); //タッチがキャンセルされキーが離された
+    }
+}
 
-////初期化
-window.onload = () => { 
+//初期化
+window.onload = () => {
     generateScale();
     window.addEventListener('keydown', handleKeyDown);
     window.addEventListener('keyup', handleKeyUp);
+    handleTouchEvents(); // タッチ対応を追加
 };
-
